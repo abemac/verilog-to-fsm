ba67c4c38b5bfdffe2d930e81a1895be22b2ce0e
Abraham McIlvaine <abemac@udel.edu>
2016-06-28 14:57:34 -0400

    asdf

--- simplifier/src/main.cpp
+++ simplifier/src/main.cpp
@@ -5,28 +5,116 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <unordered_set>
+#include <algorithm>
+
 
 
 
 void read_in_file();
+void eliminate_dup_cols();
+void del_unreachable_states();
+void write_file();
 
+
+
+std::string path;
+std::string output_path;
 std::vector<std::vector<unsigned long long> > src_file;
 
-int main(){
+int main(int argc, char* argv[]){
+  path=argv[1];
+  output_path.assign(path);
+  output_path.pop_back();
+  output_path.pop_back();
+  output_path.pop_back();
+  output_path.pop_back();
+  output_path.push_back('_');
+  output_path.push_back('s');
+  output_path.push_back('i');
+  output_path.push_back('m');
+  output_path.push_back('p');
+  output_path.push_back('.');
+  output_path.push_back('c');
+  output_path.push_back('s');
+  output_path.push_back('v');
   read_in_file();
-  for(std::vector<unsigned long long> v : src_file){
-    for(unsigned long long ull : v){
-      std::cout<<ull<<" ";
+  eliminate_dup_cols();
+  del_unreachable_states();
+
+
+  write_file();
+  std::cout<<output_path<<" outputed"<<std::endl;
+
+}
+void write_file(){
+  std::ofstream FILE;
+  FILE.open(output_path);
+  for (size_t i = 0; i < src_file.size(); i++) {
+    for (size_t j = 0; j < src_file[i].size(); j++) {
+      FILE<<src_file[i][j];
+      if(j!=(src_file[i].size()-1)){
+        FILE<<",";
+      }else{
+        FILE<<"\n";
+      }
     }
-    std::cout<<""<<std::endl;
   }
+
+  FILE.close();
+}
+
+
+void del_unreachable_states(){
+  bool first_deleted=false;
+  std::unordered_set< unsigned long long> reachable;
+  for(std::vector<unsigned long long> v : src_file ){
+    for(unsigned long long ull: v){
+      reachable.insert(ull);
+    }
+  }
+
+  unsigned long long i=0;
+  while(i<src_file.size()){
+    if(!reachable.count(i)){
+      if(!first_deleted){
+        first_deleted=true;
+      }else{
+        src_file.erase(src_file.begin()+i);
+      }
+
+    }
+    i++;
+  }
+
 
 
 }
 
+void eliminate_dup_cols(){
+  for(size_t i=0;i<src_file[0].size()-1;i++){//i is the column index
+    for(size_t j=i+1;j<src_file[0].size();j++){//j is index of all cols to the right
+      bool same=true;
+      for(size_t k=0;k<src_file.size() && same;k++){//k is the row index for each col to compare
+        if(src_file[k][i] != src_file[k][j]){
+          same=false;
+        }
+
+      }
+
+      if(same){
+        for(size_t l=0;l<src_file.size();l++){
+          src_file[l].erase(src_file[l].begin()+j);
+        }
+        j--;
+      }
+    }
+  }
+
+}
 
 void read_in_file(){
-  std::ifstream FILE("s27.csv");
+  std::ifstream FILE(path);
   std::string str;
 
   while(std::getline(FILE,str)){
