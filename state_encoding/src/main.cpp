12de201fd66f41f5c9260f274f9d79eea63c75cc
Abraham McIlvaine <abemac@udel.edu>
2016-07-13 14:36:00 -0400

    asdf

--- state_encoding/src/main.cpp
+++ state_encoding/src/main.cpp
@@ -20,25 +20,71 @@ PRIORITIZED ADJACENT
 #include <fstream>
 
 void read_in_file();
+void getEdges();
 
 std::vector<std::vector<unsigned long long> > src_file;
 std::string path;
+std::string input_freqs_path;
 std::string output_path;
+struct Edge{
+  unsigned long long start;
+  unsigned long long end;
+  unsigned long long count;
+  unsigned long long input;
+  double freq;
+};
+std::vector<Edge*> edges;
+std::vector<double> input_freqs;
 
 int main(int argc, char* argv[]){
   path="../complete/";
   path.append(argv[1]);
+  input_freqs_path="input_data/";
+  input_freqs_path.append(argv[1]);
+  input_freqs_path.append(".inputdata");
   read_in_file();
-  for(std::vector<unsigned long long > v : src_file){
-    for(unsigned long long u: v){
-      std::cout<<u<<" ";
-    }
-    std::cout<<""<<std::endl;
-  }
+  // for(std::vector<unsigned long long > v : src_file){
+  //   for(unsigned long long u: v){
+  //     std::cout<<u<<" ";
+  //   }
+  //   std::cout<<""<<std::endl;
+  // }
+  getEdges();
+
 
 }
 
+///start ::  get doubles from .inputdata and incorperate into setting freqencies
 
+void getEdges(){
+  std::vector< std::vector<int> > inserted;
+  for(unsigned long long i=0;i<src_file.size();i++){
+    inserted.push_back(std::vector<int>(src_file.size()));
+    for(unsigned long long h = 0;h<inserted.back().size();h++){
+      inserted.back()[h]=-1;
+    }
+  }
+
+  for(unsigned long long j=0;j<src_file.size();j++){
+    for(unsigned long long k=0;k<src_file[j].size();k++){
+      if(inserted[j][src_file[j][k]]==-1){
+        Edge* e = new Edge();
+        e->start=j;
+        e->end=src_file[j][k];
+        e->count=1;
+        inserted[j][src_file[j][k]]=edges.size();
+        //inserted[src_file[j][k]][j]=edges.size();
+        edges.push_back(e);
+      }else{
+        edges[inserted[j][src_file[j][k]]]->count++;
+      }
+    }
+  }
+  for(Edge* e : edges){
+    std::cout<<"{"<<e->start<<","<<e->end<<"} Count:"<<e->count<<std::endl;
+  }
+
+}
 
 void read_in_file(){
   std::ifstream FILE(path);
